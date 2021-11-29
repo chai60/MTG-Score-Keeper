eb4abbc2073fdeab1d4345cfb400be1f35d9de68
Chai Chuencharoenwong <30678488+chai60@users.noreply.github.com>
2021-11-28 22:49:25 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,78 +1,50 @@
- //Point tracker
- const p1Button = document.querySelector("#p1Button");
- const p2Button = document.querySelector("#p2Button");
- const p1Display = document.querySelector("#p1Display");
- const p2Display = document.querySelector("#p2Display");
- const resetButton = document.querySelector("#reset");
- const winningScoreSelect = document.querySelector("#playto");
+        const playerOneHp = document.getElementById("hpOne");
+        const playerTwoHp = document.getElementById("hpTwo");
+        const minusBtnP1 = document.getElementById("minusHpOne");
+        const plusBtnP1 = document.getElementById("plusHpOne");
+        const minusBtnP2 = document.getElementById("minusHpTwo");
+        const plusBtnP2 = document.getElementById("plusHpTwo");
+        const resetBtn = document.getElementById("resetGame");
 
- let p1Score = 0;
- let p2Score = 0;
- let resetScore = 0;
- let winningScore = 3;
- let isGameOver = false;
+        let lifePoint = 20;
+
+        //Life Point Tracker
+        minusBtnP1.addEventListener("click", function() {
+            lifePoint-= 1;
+            playerOneHp.textContent = lifePoint;
+        });
+
+        minusBtnP2.addEventListener("click", function() {
+            lifePoint-= 1;
+            playerTwoHp.textContent = lifePoint;
+        })
+
+        plusBtnP1.addEventListener("click", function() {
+            lifePoint+= 1;
+            playerOneHp.textContent = lifePoint;
+        })
+
+        plusBtnP2.addEventListener("click", function() {
+            lifePoint+= 1;
+            playerTwoHp.textContent = lifePoint;
+        })
+
+        resetBtn.addEventListener("click", function() {
+            reset();
+        })
+
+        //Reseting the Tracker
+        function reset() {
+            let lifePoint = 20;
+            playerOneHp.textContent = lifePoint;
+            playerTwoHp.textContent = lifePoint;
+        }
+
+        //Event log list
+        const textLog = document.getElementById("textBox");
+        const subBtn = document.getElementById("enterLog");
+        const undoLog = document.getElementById("undoLog");
 
 
- p1Button.addEventListener("click", function() {
-     if (!isGameOver) {
-         p1Score += 1;
-     if (p1Score === winningScore) {
-         isGameOver = true;
-         p1Display.classList.add("winner");
-         p2Display.classList.add("loser");
-     } p1Display.textContent = p1Score;
 
- }
- });
-
- p2Button.addEventListener("click", function() {
-     if (!isGameOver) {
-         p2Score += 1;
-     if (p2Score === winningScore) {
-         isGameOver = true;
-         p2Display.classList.add("winner");
-         p1Display.classList.add("loser");
-     } p2Display.textContent = p2Score;
- }
- });
-
- winningScoreSelect.addEventListener("change", function() {
-     winningScore = parseInt(this.value);
-     reset();
- })
-
- /*resetButton.addEventListener("click", function() {
-     p1Display.textContent = resetScore;
-     p2Display.textContent = resetScore;
-     winningScoreSelect = ;
- }); */
-
- resetButton.addEventListener("click", reset)
-
- function reset() {
-     isGameOver = false;
-     p1Score = 0;
-     p2Score = 0;
-     p1Display.textContent = 0;
-     p2Display.textContent = 0;
-     p1Display.classList.remove("winner", "loser");
-     p2Display.classList.remove("winner", "loser");           
- };
-
- //Text Log
-
- const textButton = document.getElementById("textButton");
- const textBox = document.getElementById("textBox");
- const listEvent = document.getElementById("eventLog");
- const undoLog = document.getElementById("remove");
- 
- function textLog() {
-    const newLog = document.createElement("li");
-    newLog.innerHTML = textbox.value;
-    listEvent.appendChild(newLog);
- }
-
- function previousLog() {
-     const prevLog = document.querySelector("li");
-     listEvent.removeChild(prevLog);
- }
+        
